e069c6dd2092cbee6c374d3dd1e54ad6f4acc133
Rizki irfan <98686896+rizkiwibu@users.noreply.github.com>
2024-03-02 12:08:20 +0700

    Update settings.js

--- library/settings.js
+++ library/settings.js
@@ -7,7 +7,7 @@ module.exports = {
 	limitPremium: 9999,
         expiredTokenDb: 3600,
 	dateLimit: "Oktober, 31 2022",
-	profilePath: "/images/profile.png",
+	profilePath: "/images/assets/images/20e0860b-4959-4188-9c53-71d84eed8adc.jpeg",
 	jwtToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ",
 	recaptchaKey: {
 		v2SiteKey: "6LdthJshAAAAAIvqTeddyZ-JU1GL_rpZENDX1vp6",
